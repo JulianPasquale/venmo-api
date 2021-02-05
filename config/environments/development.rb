bc33e45afa37784cabf8029cfca7bd5e373582e6
Julian Pasquale <julianagustin78@gmail.com>
2021-02-05 20:58:00 -0300

    Log ActiveRecord queries to STDOUT

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Log SQL queries to stdout for development.
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
