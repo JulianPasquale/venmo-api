6f60fa89c75b3570e28c206e473c586c5aa76cdd
Julian Pasquale <julianagustin78@gmail.com>
2021-02-06 19:15:35 -0300

    Add tests for create payment params validator

--- spec/support/shared_examples/create_payment/failure_validations.rb
+++ spec/support/shared_examples/create_payment/failure_validations.rb
@@ -14,3 +14,16 @@ RSpec.shared_context 'a failed validation' do
     expect(context.errors).to match(validator.errors.full_messages)
   end
 end
+
+RSpec.shared_context 'an invalid parameter' do |field_name|
+  it 'is not valid' do
+    expect(subject).to_not be_valid
+  end
+
+  it 'includes field with error' do
+    subject.validate
+
+    expect(subject.errors).to_not be_empty
+    expect(subject.errors.attribute_names).to match([field_name.to_sym])
+  end
+end
