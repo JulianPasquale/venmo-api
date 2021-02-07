15f7fdc8a0ac959bd2aae0ad3f8a48f19307ad4a
Julian Pasquale <julianagustin78@gmail.com>
2021-02-07 18:31:48 -0300

    Add pagination to feeds endpoint

--- app/interactors/users/feed/build_response.rb
+++ app/interactors/users/feed/build_response.rb
@@ -8,13 +8,30 @@ module Users
       def call
         context.status = :ok
 
-        context.data = data
+        context.data = response_data
       end
 
       private
 
+      def response_data
+        {
+          data: data,
+          metadata: {
+            page: page_number,
+            per_page: data.limit_value,
+            total_pages: data.total_pages
+          }
+        }
+      end
+
       def data
-        PaymentsQuery.new.second_level_friends_payments(user_id: context.user)
+        @data ||= PaymentsQuery.new.second_level_friends_payments(
+          user_id: context.user
+        ).page(page_number)
+      end
+
+      def page_number
+        (context.page || 1).to_i
       end
     end
   end
