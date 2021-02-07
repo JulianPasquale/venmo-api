532055fb962a6c27714ae690dc2ad72ee048da2f
Julian Pasquale <julianagustin78@gmail.com>
2021-02-07 12:19:41 -0300

    Add friends_up_to_second_leven query

--- app/queries/users_query.rb
+++ app/queries/users_query.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class UsersQuery < BaseQuery
-  def friends(user_id:, scope: false)
+  def friends(user_id:, scope: false, relation: @relation)
     query = relation.joins(friends_query(scope: scope)).where.not(id: user_id)
 
     query.where(friendships: { user_id: user_id }).or(
       query.where(friendships: { friend_id: user_id })
     )
+  end
+
+  def friends_up_to_second_level(user_id:, relation: @relation)
+    first_level_friends = friends(user_id: user_id)
+    friends(user_id: first_level_friends).unscope(where: :id).distinct
   end
 
   private
--- spec/queries/users_query_spec.rb
+++ spec/queries/users_query_spec.rb
@@ -64,4 +64,35 @@ RSpec.describe UsersQuery do
       expect(subject.send(:model_name)).to match(User)
     end
   end
+
+  describe '#friends_up_to_second_level' do
+    let!(:user) { create(:user) }
+    let!(:first_level_friendships) { create_list(:friendship, 2, user: user) }
+    let!(:second_level_friendships1) { create_list(:friendship, 2, user: first_level_friendships.first.friend) }
+    let!(:second_level_friendships2) { create(:friendship, user: first_level_friendships.second.friend) }
+
+    let(:expected_result) do
+      first_level_friends = user.friends
+      second_level_friends = first_level_friends.map(&:friends).flatten
+
+      [user] + first_level_friends + second_level_friends
+    end
+
+    it 'includes user direct friends' do
+      expect(subject.friends_up_to_second_level(user_id: user)).to include(*user.friends)
+    end
+
+    it "includes user's friends friends (second level)" do
+      expect(subject.friends_up_to_second_level(user_id: user)).to(
+        include(*first_level_friendships.first.friend.friends)
+      )
+      expect(subject.friends_up_to_second_level(user_id: user)).to(
+        include(*first_level_friendships.second.friend.friends)
+      )
+    end
+
+    it 'matchs with expected friends list' do
+      expect(subject.friends_up_to_second_level(user_id: user)).to match(expected_result)
+    end
+  end
 end
